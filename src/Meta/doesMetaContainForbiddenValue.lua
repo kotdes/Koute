08be498f8ff4bf9a0436d1d7f0d739a63da324a1
7kayoh <git@7kayoh.net>
2022-08-24 15:37:50 +0900

    Create doesMetaContainForbiddenValue

---
+++ src/Meta/doesMetaContainForbiddenValue.lua
@@ -0,0 +1,8 @@
+local FORBIDDEN_META_VALUES = {"Instance", "Function"}
+
+local function doesMetaContainForbiddenValue(value)
+    local forbiddenMetaType = table.find(FORBIDDEN_META_VALUES, typeof(value))
+    assert(forbiddenMetaType, forbiddenMetaType .. " is unacceptable in Koute.Meta")
+end
+
+return doesMetaContainForbiddenValue
