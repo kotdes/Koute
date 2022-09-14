2988f98aded843aa50b3df0f7532409cba0a4d5d
7kayoh <hello@7kayoh.net>
2022-09-14 19:36:33 +0900

    Assert should be printing out the type of the value

--- src/Meta/doesMetaContainForbiddenValue.lua
+++ src/Meta/doesMetaContainForbiddenValue.lua
@@ -2,7 +2,7 @@ local FORBIDDEN_META_VALUES = {"Instance", "Function"}
 
 local function doesMetaContainForbiddenValue(value)
     local forbiddenMetaType = table.find(FORBIDDEN_META_VALUES, typeof(value))
-    assert(forbiddenMetaType, forbiddenMetaType .. " is unacceptable in Koute.Meta")
+    assert(forbiddenMetaType, typeof(forbiddenMetaType) .. " is unacceptable in Koute.Meta")
 end
 
 return doesMetaContainForbiddenValue
