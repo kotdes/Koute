be23fb9ec6c3d5051fc675fa1330fbb40e94bedd
7kayoh <git@7kayoh.net>
2024-02-02 23:00:53 +0800

    Fixed faulty logic

--- src/Meta/doesMetaContainForbiddenValue.lua
+++ src/Meta/doesMetaContainForbiddenValue.lua
@@ -2,7 +2,7 @@ local FORBIDDEN_META_VALUES = {"Instance", "Function"}
 
 local function doesMetaContainForbiddenValue(value)
     local forbiddenMetaType = table.find(FORBIDDEN_META_VALUES, typeof(value))
-    assert(forbiddenMetaType, typeof(forbiddenMetaType) .. " is unacceptable in Koute.Meta")
+    assert(not forbiddenMetaType, typeof(forbiddenMetaType) .. " is unacceptable in Koute.Meta")
 end
 
 return doesMetaContainForbiddenValue
